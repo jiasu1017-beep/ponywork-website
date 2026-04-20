173fb4c91d7a8f45d9914222154d83db4b799a0c
jiasu <45795504+Haiezan@users.noreply.github.com>
2026-04-20 12:32:55 +0800

    chore(release): update version to v1.0.8

--- build_and_release_en.bat
+++ build_and_release_en.bat
@@ -5,7 +5,7 @@ REM Switch to script directory
 cd /d "%~dp0"
 
 REM Set version and release directory
-set VERSION=v1.0.7
+set VERSION=v1.0.8
 set RELEASE_DIR=release-%VERSION%
 
 echo ========================================
